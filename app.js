e94cbc7beea53b42fd3712e1b4c6ea90fca048f0
Jerrod Varney <jerrodvarney@gmail.com>
2023-01-03 11:55:15 -0800

    finish solo work and create new React components.

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+  const [isHover, setWeight] = useState(false);
+  const style = {
+    cursor: 'pointer',
+    fontWeight: isHover ? 'bold' : 'normal'
+  };
+
+  return (
+     <li style={style} onMouseEnter={() => setWeight(!isHover)} onMouseLeave={() => setWeight(!isHover)}>
+      {props.item}
+     </li>
+  );
+};
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+);
+
+var list = {};
+list.items = ['Eggs', 'Bread', 'Sandwiches', 'Kale', 'Milk'];
+
+ReactDOM.render(<GroceryList {...list} />, document.getElementById('app'));
